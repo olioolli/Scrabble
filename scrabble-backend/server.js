0a39dfdddb7ac2576047ce99fc6ac4a12f74a3ab
Olli <olli.helminen@hotmail.com>
2022-02-12 14:47:06 +0200

    initial commit

---
+++ scrabble-backend/server.js
@@ -0,0 +1,242 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TileType = void 0;
+// Importing module
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var WebSocket = __importStar(require("ws"));
+var http = __importStar(require("http"));
+var app = (0, express_1.default)();
+var PORT = 5000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+//initialize a simple http server
+var server = http.createServer(app);
+//initialize the WebSocket server instance
+var wss = new WebSocket.Server({ server: server });
+wss.on('connection', function (ws) {
+    ws.isAlive = true;
+    ws.on('pong', function () {
+        ws.isAlive = true;
+    });
+    ws.on('message', function (message) {
+        if (message === "gamestate") {
+        }
+        ws.send("Received message: ".concat(message));
+    });
+});
+server.listen(process.env.PORT || 8999, function () {
+    console.log("Listening...");
+});
+setInterval(function () {
+    wss.clients.forEach(function (ws) {
+        var extWs = ws;
+        if (!extWs.isAlive)
+            return extWs.terminate();
+        extWs.isAlive = false;
+        extWs.ping(null, false);
+    });
+}, 10000);
+var broadCastGameState = function () {
+    wss.clients.forEach(function (ws) {
+        var extWs = ws;
+        if (!extWs.isAlive)
+            return;
+        extWs.send(JSON.stringify(gameState));
+    });
+};
+// Handling GET / Request
+app.get('/', function (req, res) {
+    res.send('Welcome to typescript backend!');
+});
+// Server setup
+app.listen(PORT, function () {
+    console.log('The application is listening '
+        + 'on port http://localhost:' + PORT);
+});
+app.post("/login", function (req, res) {
+    if (!req.body.password || req.body.password !== password) {
+        res.sendStatus(401);
+        return;
+    }
+    if (req.body.username) {
+        if (!addUser(req.body.username)) {
+            res.sendStatus(400);
+            return;
+        }
+        else {
+            res.sendStatus(200);
+            return;
+        }
+    }
+    res.send(500);
+});
+app.get("/isLoggedIn", function (req, res) {
+    try {
+        var username = req.query.username;
+        var result = getUser(username) != undefined;
+        res.send(JSON.stringify({ isLoggedIn: result }));
+    }
+    catch (err) {
+        res.send(JSON.stringify({ isLoggedIn: false }));
+    }
+});
+app.post("/game", function (req, res) {
+    gameState = req.body.game;
+    broadCastGameState();
+    res.status(200).json(gameState);
+});
+app.get("/game", function (req, res) {
+    res.send(JSON.stringify(gameState));
+});
+app.get("/users", function (req, res) {
+    res.send(JSON.stringify(users));
+});
+app.get("/reset", function (req, res) {
+    gameState = initialGameState;
+    res.send("Game reset");
+});
+var addUser = function (username) {
+    if (getUser(username))
+        return false;
+    users.push(username);
+    return true;
+};
+var getUser = function (username) {
+    for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+        var user = users_1[_i];
+        if (user === username)
+            return user;
+    }
+    return undefined;
+};
+var password = "kala";
+var users = [];
+var TileType;
+(function (TileType) {
+    TileType[TileType["N0R"] = 0] = "N0R";
+    TileType[TileType["X2W"] = 1] = "X2W";
+    TileType[TileType["X3W"] = 2] = "X3W";
+    TileType[TileType["X2L"] = 3] = "X2L";
+    TileType[TileType["X3L"] = 4] = "X3L";
+    TileType[TileType["CNT"] = 5] = "CNT";
+})(TileType = exports.TileType || (exports.TileType = {}));
+var getTileTypeFromNumber = function (id) {
+    if (id === 0)
+        return TileType.N0R;
+    else if (id === 1)
+        return TileType.X2W;
+    else if (id === 2)
+        return TileType.X3W;
+    else if (id === 3)
+        return TileType.X2L;
+    else if (id === 4)
+        return TileType.X3L;
+    else if (id === 5)
+        return TileType.CNT;
+    return N0R;
+};
+var letterTileIdCounter = 0;
+var createNewLetterTile = function (character, points, owner) {
+    return {
+        letter: character,
+        points: points,
+        owner: owner,
+        id: letterTileIdCounter++
+    };
+};
+var createNewLettersTiles = function (character, points, count) {
+    var retArr = [];
+    for (var i = 0; i < count; i++)
+        retArr.push(createNewLetterTile(character, points, ""));
+    return retArr;
+};
+var pouchLetters = __spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray(__spreadArray([], createNewLettersTiles("A", 1, 10), true), createNewLettersTiles("B", 10, 10), true), createNewLettersTiles("D", 7, 10), true), createNewLettersTiles("E", 2, 10), true), createNewLettersTiles("F", 10, 10), true), createNewLettersTiles("G", 5, 10), true), createNewLettersTiles("H", 4, 10), true), createNewLettersTiles("I", 1, 10), true), createNewLettersTiles("J", 4, 10), true), createNewLettersTiles("K", 3, 10), true);
+var N0R = 0, X2W = 1, X3W = 2, X2L = 3, X3L = 4, CNT = 5;
+var gameBoardTemplate = [
+    [X3W, N0R, N0R, X2W, N0R, N0R, N0R, X3W, N0R, N0R, X2W, N0R, N0R, N0R, X3W],
+    [N0R, X2W, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X2W, N0R],
+    [N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R],
+    [N0R, N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R, N0R],
+    [N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R],
+    [N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R],
+    [N0R, N0R, X2L, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2L, N0R, N0R],
+    [X3W, N0R, N0R, X2L, N0R, N0R, N0R, CNT, N0R, N0R, N0R, X2L, N0R, N0R, X3W],
+    [N0R, N0R, X2L, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2L, N0R, N0R],
+    [N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R],
+    [N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R],
+    [N0R, N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R, N0R],
+    [N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R],
+    [N0R, X2W, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X2W, N0R],
+    [X3W, N0R, N0R, N0R, N0R, N0R, N0R, X3W, N0R, N0R, N0R, N0R, N0R, N0R, X3W]
+];
+var generateEmptyGameBoard = function () {
+    var board = [];
+    for (var x = 0; x < 15; x++) {
+        var arr = [];
+        board[x] = arr;
+        for (var y = 0; y < 15; y++) {
+            var tileType = gameBoardTemplate[x][y];
+            arr.push({
+                tileType: getTileTypeFromNumber(tileType),
+                letter: undefined,
+                isTemp: false,
+                points: undefined
+            });
+        }
+    }
+    return board;
+};
+var gameBoard = generateEmptyGameBoard();
+var initialGameState = {
+    playerPoints: {
+        "Player1": 0,
+        "Player2": 0
+    },
+    turnOfPlayer: "Player1",
+    pouchLetters: pouchLetters,
+    playerHands: {
+        "Player1": [
+            createNewLetterTile("G", 12, "Player1"),
+            createNewLetterTile("O", 1, "Player1"),
+            createNewLetterTile("D", 5, "Player1"),
+        ],
+        "Player2": [
+            createNewLetterTile("Ö", 9, "Player2"),
+        ]
+    },
+    board: gameBoard
+};
+var gameState = initialGameState;
---
+++ scrabble-backend/server.ts
@@ -0,0 +1,290 @@
+// Importing module
+import express from 'express';
+import { send } from 'process';
+import cors from 'cors';
+import * as WebSocket from 'ws';
+import * as http from 'http';
+
+const app = express();
+const PORT: Number = 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+interface ExtWebSocket extends WebSocket {
+    isAlive : boolean;
+}
+
+wss.on('connection', (ws : ExtWebSocket) => {
+
+    ws.isAlive = true;
+
+    ws.on('pong', () => {
+        ws.isAlive = true;
+    });
+
+    ws.on('message', (message: string) => { 
+       if( message === "gamestate" ) {
+
+       }
+       
+       ws.send(`Received message: ${message}`);
+    });
+});
+
+server.listen(process.env.PORT || 8999, () => {
+    console.log("Listening...");
+});
+
+setInterval(() => {
+    wss.clients.forEach((ws) => {
+        const extWs = ws as ExtWebSocket;
+        if (!extWs.isAlive) return extWs.terminate();
+        
+        extWs.isAlive = false;
+        extWs.ping(null, false);
+    });
+}, 10000);
+
+const broadCastGameState = () => {
+    wss.clients.forEach((ws) => {
+        const extWs = ws as ExtWebSocket;
+        if (!extWs.isAlive) return;
+        extWs.send(JSON.stringify(gameState));
+    });
+};
+
+// Handling GET / Request
+app.get('/', (req, res) => {
+    res.send('Welcome to typescript backend!');
+})
+
+// Server setup
+app.listen(PORT, () => {
+    console.log('The application is listening '
+        + 'on port http://localhost:' + PORT);
+})
+
+app.post("/login", (req, res) => {
+    if (!req.body.password || req.body.password !== password) {
+        res.sendStatus(401);
+        return;
+    }
+
+    if (req.body.username) {
+        if (!addUser(req.body.username)) {
+            res.sendStatus(400);
+            return;
+        }
+        else {
+            res.sendStatus(200);
+            return;
+        }
+    }
+    res.send(500);
+});
+
+app.get("/isLoggedIn", (req, res) => {
+    try {
+        const username = req.query.username as string;
+        const result = getUser(username) != undefined;
+        res.send(JSON.stringify({ isLoggedIn: result }));
+    }
+    catch (err) {
+        res.send(JSON.stringify({ isLoggedIn: false }));
+    }
+});
+
+app.post("/game", (req, res) => {
+    gameState = req.body.game;
+    broadCastGameState();
+    res.status(200).json(gameState);
+});
+
+app.get("/game", (req, res) => {
+    res.send(JSON.stringify(gameState));
+});
+
+app.get("/users", (req, res) => {
+    res.send(JSON.stringify(users));
+});
+
+app.get("/reset", (req, res) => {
+    gameState = initialGameState;
+    res.send("Game reset");
+});
+
+const addUser = (username: string) => {
+    if (getUser(username))
+        return false;
+
+    users.push(username);
+    return true;
+}
+
+const getUser = (username: string) => {
+    for (let user of users)
+        if (user === username)
+            return user;
+
+    return undefined;
+}
+
+const password = "kala";
+const users: string[] = [];
+
+export enum TileType {
+    N0R = 0,
+    X2W = 1,
+    X3W = 2,
+    X2L = 3,
+    X3L = 4,
+    CNT = 5
+}
+
+const getTileTypeFromNumber = (id: number) => {
+    if (id === 0)
+        return TileType.N0R;
+    else if (id === 1)
+        return TileType.X2W;
+    else if (id === 2)
+        return TileType.X3W;
+    else if (id === 3)
+        return TileType.X2L;
+    else if (id === 4)
+        return TileType.X3L;
+    else if (id === 5)
+        return TileType.CNT;
+
+    return N0R;
+}
+
+let letterTileIdCounter = 0;
+
+export type LetterTile = {
+    letter: string,
+    points : number,
+    owner: string
+    id: number
+}
+
+const createNewLetterTile = (character: string, points : number, owner : string) => {
+    return {
+        letter: character,
+        points: points,
+        owner : owner,
+        id : letterTileIdCounter++
+    }
+}
+
+const createNewLettersTiles = (character: string, points : number, count : number) : LetterTile[] => {
+    let retArr = [];
+    for(let i =0; i < count; i++)
+        retArr.push(createNewLetterTile(character,points,""));
+    return retArr;
+}
+
+const pouchLetters = [
+    ...createNewLettersTiles("A",1,10),
+    ...createNewLettersTiles("B",10,10),
+    ...createNewLettersTiles("D",7,10),
+    ...createNewLettersTiles("E",2,10),
+    ...createNewLettersTiles("F",10,10),
+    ...createNewLettersTiles("G",5,10),
+    ...createNewLettersTiles("H",4,10),
+    ...createNewLettersTiles("I",1,10),
+    ...createNewLettersTiles("J",4,10),
+    ...createNewLettersTiles("K",3,10),
+];
+
+export type BoardTile = {
+    tileType: TileType,
+    letter: string | undefined,
+    points: number | undefined,
+    // Placed letter is temp until turn passed
+    isTemp: boolean
+}
+
+export type GameState = {
+    playerPoints : Record<string,number>,
+    turnOfPlayer : string,
+    pouchLetters: LetterTile[]
+    board : BoardTile[][],
+    playerHands: Record<string, LetterTile[]>
+}
+
+const N0R = 0,
+    X2W = 1,
+    X3W = 2,
+    X2L = 3,
+    X3L = 4,
+    CNT = 5;
+
+let gameBoardTemplate =
+    [
+        [X3W, N0R, N0R, X2W, N0R, N0R, N0R, X3W, N0R, N0R, X2W, N0R, N0R, N0R, X3W],
+        [N0R, X2W, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X2W, N0R],
+        [N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R],
+        [N0R, N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R, N0R],
+        [N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R],
+        [N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R],
+        [N0R, N0R, X2L, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2L, N0R, N0R],
+        [X3W, N0R, N0R, X2L, N0R, N0R, N0R, CNT, N0R, N0R, N0R, X2L, N0R, N0R, X3W],
+        [N0R, N0R, X2L, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2L, N0R, N0R],
+        [N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R],
+        [N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R, N0R, X2W, N0R, N0R, N0R, N0R],
+        [N0R, N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R, N0R],
+        [N0R, N0R, X2W, N0R, N0R, N0R, X2L, N0R, X2L, N0R, N0R, N0R, X2W, N0R, N0R],
+        [N0R, X2W, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X3L, N0R, N0R, N0R, X2W, N0R],
+        [X3W, N0R, N0R, N0R, N0R, N0R, N0R, X3W, N0R, N0R, N0R, N0R, N0R, N0R, X3W]
+    ]
+
+const generateEmptyGameBoard = () => {
+
+    const board: BoardTile[][] = [];
+
+    for (let x = 0; x < 15; x++) {
+        const arr: BoardTile[] = [];
+        board[x] = arr;
+        for (let y = 0; y < 15; y++) {
+            const tileType = gameBoardTemplate[x][y];
+            arr.push({
+                tileType: getTileTypeFromNumber(tileType),
+                letter: undefined,
+                isTemp: false,
+                points: undefined
+            });
+        }
+    }
+
+    return board;
+};
+
+let gameBoard = generateEmptyGameBoard();
+const initialGameState : GameState = {
+    playerPoints: {
+        "Player1" : 0,
+        "Player2" : 0
+    },
+    turnOfPlayer: "Player1",
+    pouchLetters: pouchLetters,
+
+    playerHands: {
+        "Player1" : [
+            createNewLetterTile("G",12,"Player1"),
+            createNewLetterTile("O",1,"Player1"),
+            createNewLetterTile("D",5,"Player1"),
+        ],
+        "Player2": [ 
+            createNewLetterTile("Ö",9,"Player2"),
+        ]
+    },
+    board: gameBoard
+};
+let gameState = initialGameState;
