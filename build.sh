ce0c959298264b6ba086f47422924d0ab70fd1fe
Olli <olli.helminen@hotmail.com>
2022-09-20 16:28:44 +0300

    refactor scripts

--- build.sh
+++ build.sh
@@ -2,16 +2,12 @@ cd my-app
 npm install
 npm install react-scripts --save
 chmod +x node_modules/.bin/react-scripts
+rm -r build/*
 REACT_APP_BE_IP=`dig +short myip.opendns.com @resolver1.opendns.com` npm run build
 
 cd ..
-chmod a+x node_modules/.bin/tsc
 cd scrabble-backend
 npm install
+rm -r build/*
+chmod a+x node_modules/.bin/tsc
 npm run build
-
-#Start backend
-forever start scrabble-backend/server.js
-
-#Start webapp
-serve -p 80 -s my-app/build
