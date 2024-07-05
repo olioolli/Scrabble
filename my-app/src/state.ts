f382f9fdc11090fa68eb5bc1b9e6a5b5b86fbfd6
Olli Helminen <olli.helminen@hotmail.com>
2024-07-05 11:46:53 +0300

    remove hardcoded be port

--- my-app/src/state.ts
+++ my-app/src/state.ts
@@ -11,10 +11,10 @@ export const usePlayers = () => {
  };
 }
 
-const BE_IP = process.env.REACT_APP_BE_IP || "localhost";
+const BE_IP = process.env.REACT_APP_BE_IP || "localhost:5000";
 
 const BE_PROTOCOL = process.env.REACT_APP_BE_PROTOCOL || 'http'
 
-export const BE_URL = BE_PROTOCOL+"://"+BE_IP+":5000";
+export const BE_URL = BE_PROTOCOL+"://"+BE_IP;
 
 export const BE_WS_URL = "ws://"+BE_IP+":8999";
